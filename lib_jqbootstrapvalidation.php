fed52e48a2d1185b1700f3d6901cc10c24b2ed86
rylon <rylon@web.de>
2015-06-22 17:04:31 +0200

    init

---
+++ lib_jqbootstrapvalidation.php
@@ -0,0 +1,10 @@
+<?php
+namespace LIB;
+class lib_jqbootstrapvalidation extends \LIB\lib_js{
+    public static function get_class(){
+        return self::class;}
+    public static function js_path(){
+        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/jqbootstrapvalidation/lib/jqBootstrapValidation.js');}
+    public static function version(){
+        return '<a href="http://ReactiveRaven.github.com/jqBootstrapValidation/" target="_blank">1.3.6</a>';}
+}
